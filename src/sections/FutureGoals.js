82c63a875d506070a84ea76afe80dae3525f95a6
Muhammad Sabir Junaid <100062048+blockchain-ekazy@users.noreply.github.com>
2022-04-01 17:59:04 +0500

    Future Goals done

--- src/sections/FutureGoals.js
+++ src/sections/FutureGoals.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import Img from "../Components/Imgs/FG.jpeg";
+import Img2 from "../Components/Imgs/FG2.png";
+
+import BIA from '../Components/Imgs/A.png'
+import BIB from '../Components/Imgs/B.png'
 
 // import ImgG from "../Components/Imgs/H2.PNG";
 
@@ -40,32 +44,25 @@ const FutureGoals = () => {
                     <strong>One</strong>
                   </h3> */}
                   <p className="p-2 m-0">
-                    {/* <strong>Schwerpunkt: Frontend-Entwicklung</strong>
-                        <br /> */}
-                    We are building bodies and outfits for other collections
-                    including World of Women Galaxy, Boss Beauties, and Fame
-                    Lady Squad. DESIGNER COLLABS We are working with emerging
-                    apparel designers to add additional traits/layers into our
-                    system to allow for more outfit options. Do you own an
-                    apparel brand? Or an accessory or jewelry company? Do you
-                    want to add your products into the next WoO collection?
-                    Contact us. USER DRIVEN OUTFITS We are building an interface
-                    where users will be able to select whatever trait/outfit
-                    they desire and to then mint that custom outfit onto their
-                    WoW or other PFP. ABILITY TO MINT OTHER BODY TYPES We want
-                    to add the ability for a user to mint different body types
-                    so we create a more diverse and realistic set of bodies.
-                    ABILITY TO MINT 3D WEARABLES BASED ON YOUR OUTFIT We want to
-                    give you a way to wear your outfit in the metaverse. If you
-                    are lucky enough to mint a legendary outfit you will receive
-                    a 3d wearable for free if the project sells out. Other
-                    holders will have the ability to mint a 3d wearable for use
-                    in Decentraland too. World of Outfits is creating a fashion
-                    platform that allows end users and designers to collab and
-                    create endless looks for the metaverse and IRL. Owning a WoO
-                    is a ticket into this world of web3 fashion and metaverse
-                    apparel, accessories, and jewelry. This is only the
-                    beginning.
+                    <strong>ADDING ADDITIONAL COLLECTIONS</strong>
+                        <br />
+                        <div className="container">
+                          <div className="row align-items-center">
+                            <div className="col-6">
+                            We are building bodies and outfits for other collections including World of Women Galaxy, Boss Beauties, and Fame Lady Squad.
+
+                            </div>
+                            <div className="col-6">
+                              <img className="w-25" src={BIA}></img>
+                              <img className="w-25" src={BIB}></img>
+                            </div>
+                            {/* <div className="col-3 m-0 p-0">
+
+                            </div> */}
+                          </div>
+                        </div>
+                   
+
                   </p>
                 </div>
               </li>
@@ -77,12 +74,10 @@ const FutureGoals = () => {
                   </p> */}
                   {/* <h3 className="RDH">
                     {" "}
-                    <strong>Two</strong>
                   </h3> */}
+                  <strong>DESIGNER COLLABS</strong>
                   <p className="p-3 m-0">
-                    USERS WILL BE ABLE TO ACTUALLY SELECT WHAT
-                    OUTFITS/TRAITS/LAYERS THEY WANT TO ADD TO THEIR BODY THUS
-                    CREATING A CUSTOM LOOK ONTO THEIR PFP.
+                  We are working with emerging apparel designers to add additional traits/layers into our system to allow for more outfit options. Do you own an apparel brand? Or an accessory or jewelry company? Do you want to add your products into the next WoO collection? Contact us.
                   </p>
                 </div>
               </li>
@@ -94,93 +89,73 @@ const FutureGoals = () => {
                   </p> */}
                   {/* <h3 className="RDH">
                     {" "}
-                    <strong>Three</strong>
                   </h3> */}
+                  <strong>USER DRIVEN OUTFITS</strong>
                   <p className="p-3 m-0">
-                    WE ARE WORKING ON BUILDING DIFFERENT BODY TYPES SO YOU CAN
-                    MINT DIFFERENT BODIES ONTO YOUR PFP. WE WANT A MORE DIVERSE
-                    AND REALISTIC SET OF BODIES FOR THE ENDLESS OUTFIT OPTIONS
-                    WE ARE CREATING. TO LAUNCH, WE HAVE CREATED ONE BODY TYPE AS
-                    IT BECOMES A BIT COMPLEX TO CREATE OUTFITS ON MULTIPLE BODY
-                    TYPES FOR THE INITIAL LAUNCH. BUT WE ARE WORKING ON IT!
-                  </p>
-                </div>
-              </li>
-              <li class="timeline-event">
-                <label class="timeline-event-icon"></label>
-                <div class="timeline-event-copy">
-                  {/* <h3 className="RDH">
-                    {" "}
-                    <strong>Four</strong>
-                  </h3> */}
-                  <p className="p-3 m-0">
-                    WE ARE BUILDING BODIES FOR A NUMBER OF POPULAR PFP
-                    COLLECTIONS. WE ARE WORKING ON BOSS BEAUTIES AND FAME LADY
-                    SQUAD NEXT. IS THERE ANOTHER PFP COLLECTION THAT YOU’D LIKE
-                    TO SEE BODIES AND OUTFITS FOR? DM US.{" "}
-                  </p>
-                </div>
-              </li>
-              <li class="timeline-event">
-                <label class="timeline-event-icon"></label>
-                <div class="timeline-event-copy">
-                  {/* <p class="timeline-event-thumbnail">
-                    <i class="fa-solid fa-dice-three"></i>
-                  </p> */}
-                  {/* <h3 className="RDH">
-                    {" "}
-                    <strong>Five</strong>
-                  </h3> */}
-                  <p className="p-3 m-0">
-                    WoO is working with VR designers to create actual 3d
-                    wearable versions of the outfits in our collection.
-                    Ultimately the goal is for you to be able to mint a wearable
-                    version of the outfit you received or designed on your own,
-                    and then to mint a 3d wearable of the outfit. This will let
-                    you stunt in the metaverse in a custom designed garment for
-                    your PFP. 6. Our end goal is to create a creative fashion
-                    platform that allows end users and designers to collab and
-                    create endless looks for the metaverse and IRL.{" "}
-                  </p>
-                </div>
-              </li>
-              <li class="timeline-event">
-                <label class="timeline-event-icon"></label>
-                <div class="timeline-event-copy">
-                  {/* <p class="timeline-event-thumbnail">
-                    <i class="fa-solid fa-dice-three"></i>
-                  </p> */}
-                  {/* <h3 className="RDH">
-                    {" "}
-                    <strong>Six</strong>
-                  </h3> */}
-                  <p className="p-3 m-0">
-                    . Our futuristic reach goal is to get to a place where we
-                    can take the digital blueprints of any of these outfits and
-                    send to a cut & sew factory partner to then create real life
-                    versions of any outfit that you dream up. We are not
-                    promising goal 7! But this is where are fashion vision and
-                    apparel dreams are headed. Welcome to the wild west of web3
-                    fashion.{" "}
-                  </p>
-                </div>
-              </li>
-              <li class="timeline-event">
-                <label class="timeline-event-icon"></label>
-                <div class="timeline-event-copy">
-                  {/* <p class="timeline-event-thumbnail">
-                    <i class="fa-solid fa-dice-three"></i>
-                  </p> */}
-                  {/* <h3 className="RDH">
-                    {" "}
-                    <strong>Seven</strong>
-                  </h3> */}
-                  <p className="p-3 m-0">
-                    3d sewing your garment and sending to you! Ok{" "}
+                  We are building an interface where users will be able to select whatever trait/outfit they desire and to then mint that custom outfit onto their WoW or other PFP.
                   </p>
                 </div>
               </li>
             </ul>
+          </div>
+        </div>
+
+        <div className="row align-items-center">
+          <div className="col-md-8">
+          <ul class="timeline my-5">
+              <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                  {/* <p class="timeline-event-thumbnail">
+                    <i class="fa-solid fa-dice-one"></i>
+                  </p> */}
+                  {/* <h3 className="RDH">
+                    {" "}
+                    <strong>One</strong>
+                  </h3> */}
+                  <p className="p-2 m-0">
+                    <strong>ABILITY TO MINT OTHER BODY TYPES </strong>
+                        <br />
+                        We want to add the ability for a user to mint different body types so we create a more diverse and realistic set of bodies.
+
+                  </p>
+                </div>
+              </li>
+              <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                  {/* <p class="timeline-event-thumbnail">
+                    <i class="fa-solid fa-dice-two"></i>
+                  </p> */}
+                  {/* <h3 className="RDH">
+                    {" "}
+                  </h3> */}
+                  <strong>ABILITY TO MINT 3D WEARABLES BASED ON YOUR OUTFIT</strong>
+                  <p className="p-3 m-0">
+                  We want to give you a way to wear your outfit in the metaverse. If you are lucky enough to mint a legendary outfit you will receive a 3d wearable for free if the project sells out. Other holders will have the ability to mint a 3d wearable for use in Decentraland too.
+                   </p>
+                </div>
+              </li>
+              <li class="timeline-event">
+                {/* <label class="timeline-event-icon"></label> */}
+                <div class="timeline-event-copy">
+                  {/* <strong>USER DRIVEN OUTFITS</strong> */}
+                  <p className="p-3 m-0">
+                  World of Outfits is creating a fashion platform that allows end users and designers to collab and create endless looks for the metaverse and IRL.<br/><br/> Owning a WoO is a ticket into this world of web3 fashion and metaverse apparel, accessories, and jewelry. This is only the beginning.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <img
+              src={Img2}
+              className=" m-auto d-none d-sm-block"
+              width="600px"
+              title=""
+              alt=""
+            />
+           
           </div>
         </div>
         {/* </Fade> */}
